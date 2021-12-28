798df6bcc0645149a110cdcd38e8d0cd8980e4d7
MaxiCorrea <jmaxicorrea@gmail.com>
2021-12-27 21:06:45 -0300

    String concatenation

---
+++ 001-the-basics/string-concatenation.js
@@ -0,0 +1,8 @@
+let firstName = "Maximiliano";
+let lastName = "Correa";
+let greeting = "Hello , " + firstName + " " + lastName;
+console.log(greeting);
+
+// Using template string
+let greetingTemplate = `Welcome ${firstName} , ${lastName} to javascript language`;
+console.log(greetingTemplate);
---
+++ 001-the-basics/typeof.js
@@ -0,0 +1,6 @@
+let x = 20;
+let y = "Hello";
+let z = {};
+console.log(typeof x); // number
+console.log(typeof y); // string
+console.log(typeof z); // object
