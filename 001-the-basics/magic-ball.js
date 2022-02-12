10fdd1ca0848df101568d6b51a8c6b6b8dbb80a3
MaxiCorrea <jmaxicorrea@gmail.com>
2022-02-12 16:16:38 -0300

    Magic Ball Exercise

---
+++ 001-the-basics/magic-ball.js
@@ -0,0 +1,10 @@
+
+const generateMagicBalls = () => {
+    let counter = 0;
+    while(counter++ < 3) {
+        let randomNumber = Math.floor( Math.random() * 11 );
+        console.log(randomNumber);
+    }
+}
+
+generateMagicBalls();
