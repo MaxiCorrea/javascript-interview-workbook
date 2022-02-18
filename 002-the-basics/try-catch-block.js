31afa6e08645794ab6045ff6a98f75d1eacd7863
Maximiliano Correa <jmaxicorrea@gmail.com>
2022-02-18 00:35:40 -0300

    Format Code

--- 002-the-basics/try-catch-block.js
+++ 002-the-basics/try-catch-block.js
@@ -1,7 +1,7 @@
 let price = 100;
 try {
-    price / discount;
-    console.log(price);
-} catch(err) {
-    console.log(`${err.name} - ${err.message}`);
+  price / discount;
+  console.log(price);
+} catch (err) {
+  console.log(`${err.name} - ${err.message}`);
 }
