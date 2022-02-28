5cb2cdb9d51440250c9ad31652e41db00cae7fe2
Maximiliano Correa <jmaxicorrea@gmail.com>
2022-02-27 22:25:20 -0300

    Creating Dinamic Keys ES6

--- 003-the-basics/objects-creation.js
+++ 003-the-basics/objects-creation.js
@@ -41,6 +41,20 @@ let employee = {
 console.log(employee);
 employee.message();
 
+// Object Literal Dinamic Keys [ES6]
+
+let num1 = 10;
+let num2 = 20;
+let num3 = 30;
+
+let numObject = {
+  [ num1 + 1 ] : num1,
+  [ num2 + 1 ] : num2,
+  [ num3 + 1 ] : num3
+}
+
+console.log(numObject);
+
 // Object Constructor
 
 // Assign() Method
