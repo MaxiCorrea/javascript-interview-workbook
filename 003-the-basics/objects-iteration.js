6850c6d95c662037496e27477e5a716ad2ce0a23
Maximiliano Correa <jmaxicorrea@gmail.com>
2022-03-06 12:28:06 -0300

    Objects Iteration Examples

--- 003-the-basics/objects-iteration.js
+++ 003-the-basics/objects-iteration.js
@@ -6,7 +6,18 @@ let objectOne = {
   age: 30,
 };
 
-for (key in objectOne) {
-  console.log(`key is : ${key}`);
-  console.log(`value is : ${objectOne[key]}`);
+for (k in objectOne) {
+  console.log(`key is : '${k}' value is : ${objectOne[k]}`);
 }
+
+let keys = Object.keys(objectOne);
+console.log(keys);
+
+let values = Object.values(objectOne);
+console.log(values);
+
+let entries = Object.entries(objectOne);
+console.log(entries);
+
+console.log(Object.keys({}).length === 0);
+console.log(Object.keys(objectOne).length === 0);
