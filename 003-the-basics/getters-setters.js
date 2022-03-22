b702032a011c026c3cbed48bb9e3eb9c017a3a97
Maximiliano Correa <jmaxicorrea@gmail.com>
2022-03-22 20:49:38 -0300

    get/set keyword, private fields in class implementation

--- 003-the-basics/getters-setters.js
+++ 003-the-basics/getters-setters.js
@@ -5,10 +5,24 @@ let trunck = {
   getColor: function () {
     return this.color;
   },
-  setColor : function (color) {
-      this.color = color;
-  }
+  setColor: function (color) {
+    this.color = color;
+  },
 };
 console.log(trunck.getColor());
 trunck.setColor("blue");
 console.log(trunck.getColor());
+
+// get & set keyword , Class with private fields
+class User {
+    #name;
+    constructor(name, age) {
+        this.name = name;
+    }
+    get name (){
+        return this.#name;
+    }
+    set name (name) {
+        this.#name = name;
+    }
+}
