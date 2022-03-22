ba3c2565ead332ff6102eb044fc72caf35a06cfd
Maximiliano Correa <jmaxicorrea@gmail.com>
2022-03-22 20:38:29 -0300

    Getters & Setters Default Method Sintax (Literal Object)

--- 003-the-basics/getters-setters.js
+++ 003-the-basics/getters-setters.js
@@ -1,9 +1,14 @@
-// Default Method Syntax
+// Default Method Syntax Getter & Setter
 
 let trunck = {
   color: "red",
   getColor: function () {
     return this.color;
   },
+  setColor : function (color) {
+      this.color = color;
+  }
 };
+console.log(trunck.getColor());
+trunck.setColor("blue");
 console.log(trunck.getColor());
