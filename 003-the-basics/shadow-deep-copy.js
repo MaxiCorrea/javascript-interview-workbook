406a4a04bf04dfca8458430e472db3ad0b24e06a
Maximiliano Correa <jmaxicorrea@gmail.com>
2022-03-22 21:39:44 -0300

    Shallow Copy For in key : value properties & Spread Operator

--- 003-the-basics/shadow-deep-copy.js
+++ 003-the-basics/shadow-deep-copy.js
@@ -6,10 +6,29 @@ console.log(number2); // 10;
 
 // Copy by reference
 let person1 = {
-    name : "Maxi",
-    surname : "Correa"
+  name: "Maxi",
+  surname: "Correa",
 };
 
 let person2 = person1;
 person2.name = "Max Power";
 console.log(person1.name); // Max Power
+
+// Shallow Copy for in  ( key : value )
+let console1 = {
+  name: "Nintendo",
+};
+let console2 = {};
+for (key in console1) {
+  console2[key] = console1[key];
+}
+console.log(console1);
+console.log(console2);
+
+// Shallow Copy Spread Operator
+let console3 = {
+  name: "Sega",
+};
+let console4 = { ...console3 };
+console.log(console3);
+console.log(console4);
