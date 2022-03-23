0d9b63255f125e7d0add27c4963d66fcdd00b754
Maximiliano Correa <jmaxicorrea@gmail.com>
2022-03-23 02:45:55 -0300

    Array Literal Notation

---
+++ 004-the-basics/array-declaration.js
@@ -0,0 +1,13 @@
+//  Array Literal Notation
+let fruitsArray = ['mango', 'apple', 'grapes', 'bluberry', 'watermelon'];
+console.log(typeof(fruitsArray));
+console.log(Array.isArray(fruitsArray));
+console.log(fruitsArray);
+console.log(fruitsArray[0]);
+console.log(fruitsArray[2]);
+console.log(fruitsArray[4]);
+
+// Array Constructor
+
+
+// Array.of Factory Method
