975dd7c120ea78db02070de453b3e93d026f27f1
Maximiliano Correa <jmaxicorrea@gmail.com>
2022-03-24 22:24:57 -0300

    Array Methods -> unshift

--- 004-the-basics/array-methods.js
+++ 004-the-basics/array-methods.js
@@ -1,7 +1,13 @@
-// Array.prototype.push 
-let array = [1,2,3];
+// Array.prototype.push
+let array = [1, 2, 3];
 console.log(array);
 array.push(4);
 console.log(array);
-array.push(5,6,7);
-console.log(array);
+array.push(5, 6, 7);
+console.log(array); // 1,2,3,4,5,6,7
+
+// Array.prototype.unshift
+let array2 = [3];
+console.log(array2);
+array2.unshift(1, 2);
+console.log(array2); // 1,2,3
