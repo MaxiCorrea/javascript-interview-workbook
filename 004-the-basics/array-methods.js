7f490e48ee60ffb6853c6ab9ad55f435bfb82fbf
Maximiliano Correa <jmaxicorrea@gmail.com>
2022-03-25 19:20:19 -0300

    Array splice method example

--- 004-the-basics/array-methods.js
+++ 004-the-basics/array-methods.js
@@ -11,3 +11,11 @@ let array2 = [3];
 console.log(array2);
 array2.unshift(1, 2);
 console.log(array2); // 1,2,3
+
+// Array.prototype.splice
+let array3 = ["a", "b", "c"];
+console.log(array3);
+array3.splice(1, 0, "1", "2");
+console.log(array3); //  a , 1 , 2 , b , c
+array3.splice(1, 2, "1", "2");
+console.log(array3); //  a , 1 , 2 , b , c
