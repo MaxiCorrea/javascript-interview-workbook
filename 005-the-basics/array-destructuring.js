9d79276b661ba52f4efadd3453b6debefc852ee1
Maximiliano Correa <jmaxicorrea@gmail.com>
2022-03-30 10:31:07 -0300

    Destructuring array function

--- 005-the-basics/array-destructuring.js
+++ 005-the-basics/array-destructuring.js
@@ -1,12 +1,20 @@
 // destructuring array
 let alphabet = ["a", "b", "c", "d", "e", "f"];
-let [first , second] =  alphabet;
+let [first, second] = alphabet;
 console.log(alphabet);
 console.log(first);
 console.log(second);
-let [ , , , , five] =  alphabet;
+let [, , , , five] = alphabet;
 console.log(alphabet);
 console.log(five);
-let [ , , , , , , seven] =  alphabet;
+let [, , , , , , seven] = alphabet;
 console.log(alphabet);
 console.log(seven); // undefined
+
+// destructuring array function
+
+function values() {
+  return ["This", "is", "a", "message"];
+}
+let [one, two, three, four] = values();
+console.log(one, two, three, four);
