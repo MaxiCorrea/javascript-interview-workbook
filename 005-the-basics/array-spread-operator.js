84408b527c88343067dee0cfb63df2516ac0067b
Maximiliano Correa <jmaxicorrea@gmail.com>
2022-04-01 12:38:47 -0300

    Format code!

--- 005-the-basics/array-spread-operator.js
+++ 005-the-basics/array-spread-operator.js
@@ -7,7 +7,7 @@ console.log(array1);
 // Merge Arrays with Spread Operator
 let array2 = [1, 2, 3];
 let array3 = [4, 5, 6];
-let array4 = [...array2, ...array3]; 
+let array4 = [...array2, ...array3];
 console.log(array2);
 console.log(array3);
 console.log(array4);
@@ -15,7 +15,7 @@ console.log(array4);
 //when dealing with larger data sets it is better use the concat() method.
 
 // Copy Arrays
-let array5 = [1,2,3,4,4,45,5,5,5];
+let array5 = [1, 2, 3, 4, 4, 45, 5, 5, 5];
 console.log(array5);
 let array5Copy = [...array5];
 console.log(array5Copy);
