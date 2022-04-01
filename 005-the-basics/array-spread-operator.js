aa36e7ce6571ae2f8cca7f4059220e82fff7f507
Maximiliano Correa <jmaxicorrea@gmail.com>
2022-04-01 08:15:59 -0300

    Copy Arrays with Spread Operator

--- 005-the-basics/array-spread-operator.js
+++ 005-the-basics/array-spread-operator.js
@@ -13,3 +13,9 @@ console.log(array3);
 console.log(array4);
 //Take note that for larger arrays with more items the spread syntax is slower. Therefore,
 //when dealing with larger data sets it is better use the concat() method.
+
+// Copy Arrays
+let array5 = [1,2,3,4,4,45,5,5,5];
+console.log(array5);
+let array5Copy = [...array5];
+console.log(array5Copy);
