26a5193401c7b9aec45d81eb4e82dc28d4dc37a0
Maximiliano Correa <jmaxicorrea@gmail.com>
2022-04-01 08:10:11 -0300

    Array Spread Operator Examples

--- 005-the-basics/array-spread-operator.js
+++ 005-the-basics/array-spread-operator.js
@@ -1,1 +1,13 @@
-// Spread Operator
+// Spread Operator (Array)
+console.log(...[1, 2, 3, 4]); // 1 2 3 4
+console.log([1, 2, 3, 4]); // [1,2,3,4]
+let array1 = [1, 2, 3, ...[4, 5]];
+console.log(array1);
+
+// Merge Arrays with Spread Operator
+let array2 = [1, 2, 3];
+let array3 = [4, 5, 6];
+let array4 = [...array2, ...array3];
+console.log(array2);
+console.log(array3);
+console.log(array4);
