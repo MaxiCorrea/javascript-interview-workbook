1b9c6a557bebb8ef6af210b77fba465329825171
Maximiliano Correa <jmaxicorrea@gmail.com>
2022-04-17 01:02:37 -0300

    Map Exercise

--- 005-the-basics/array-map-reducer-filter.js
+++ 005-the-basics/array-map-reducer-filter.js
@@ -6,3 +6,8 @@ let multiplied = numbers0.map((value, index, array) => {
 console.log(numbers0); //[ 1, 2, 3, 4, 5 ]
 console.log(multiplied); //[ 6, 12, 18, 24, 30 ]
 
+let doubleReverse = numbers0.map((value , index , array) => {
+  return (value * 2);
+}).reverse();
+console.log(doubleReverse);  // [ 10, 8, 6, 4, 2 ]
+
