c0fe62fb2dc5a45bc61779cdd219919ffdb40a91
Maximiliano Correa <jmaxicorrea@gmail.com>
2022-04-23 17:59:52 -0300

    Multimencional Arrays

--- 005-the-basics/array-multi-dimensional.js
+++ 005-the-basics/array-multi-dimensional.js
@@ -1,1 +1,19 @@
 // Multi-Dimensional Arrays!
+let arra1 = [1, 2, 3, 4, 5];
+let arra2 = [6, 7, 8, 9, 10];
+let matrix1 = [arra1, arra2];
+console.log(matrix1);
+
+let matrix2 = [
+  [1, 2, 3, 4, 5],
+  [6, 7, 8, 9, 10],
+];
+console.log(matrix2);
+
+// Iteration
+for( let rowIdx = 0 ; rowIdx < matrix2.length ; ++rowIdx) {
+  let rowData = matrix2[rowIdx];
+  for( let colIdx = 0; colIdx < rowData.length ; ++colIdx) {
+    console.log(`matrix[${rowIdx}][${colIdx}] = ${matrix2[rowIdx][colIdx]}`)
+  }
+}
