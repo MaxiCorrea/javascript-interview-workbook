e156c2ded95d4333ebfb85b01c096bec49bb1d47
Maximiliano Correa <jmaxicorrea@gmail.com>
2022-05-17 21:34:56 -0300

    isOdd And isPrime Functions

--- 006-the-basics/functions-as-objects.js
+++ 006-the-basics/functions-as-objects.js
@@ -19,8 +19,32 @@ const sum = (a, b, c) => a + b + c;
 console.log(sum(2, 3, 4));
 
 // Arguments Parameters
-function game  (a ,b ,c)  {
+function game(a, b, c) {
   console.log("Arguments " + arguments.length);
 }
 game();
-game(1,3,4);
+game(1, 3, 4);
+
+// Exercises
+
+function isOdd(x) {
+  return x % 2 !== 0;
+}
+
+console.log(isOdd(1));
+console.log(isOdd(2));
+console.log(isOdd(3));
+console.log(isOdd(4));
+
+function isPrime(x) {
+    for( let y = 1 ; y < x ; ++y) 
+      if(x % 2 === 0) return false;
+    return true;  
+}
+console.log();
+
+console.log(isPrime(1));
+console.log(isPrime(2));
+console.log(isPrime(3));
+console.log(isPrime(4));
+console.log(isPrime(5));
