14d1c817bd0f81b804e090537da893f1181338b9
Maximiliano Correa <jmaxicorrea@gmail.com>
2022-05-19 00:06:27 -0300

    setInterval function expression example

--- 006-the-basics/function-expressions.js
+++ 006-the-basics/function-expressions.js
@@ -9,3 +9,10 @@ let multiply = function (a, b, c) {
   return a * b * c;
 };
 console.log(multiply(2, 20, 10));
+
+let handler = function () {
+  const date = new Date();
+  const time = date.toLocaleTimeString();
+  console.log(time);
+}
+let timer = setInterval(handler, 1000);
