eee2702b99d233918c077e9ff10679e774b67e52
Maximiliano Correa <jmaxicorrea@gmail.com>
2022-05-29 17:12:09 -0300

    IIFE functions!

--- 006-the-basics/immediately-invoked-functions-expression.js
+++ 006-the-basics/immediately-invoked-functions-expression.js
@@ -1,4 +1,25 @@
 //Immediately invoked function expressions (IIFE)
 (function () {
-    console.log("run immediately");
-  })();
+  console.log("run immediately");
+})();
+
+// IIFE example
+(function (a, b) {
+  console.log(`${a} + ${b} is ${a + b}`);
+})(50, 15);
+
+// Exercises
+let person = "Rena";
+(function () {
+  let person = "Robbie";
+  console.log(person);
+})();
+console.log(person);
+
+// Exercises
+let x = 10;
+(function (x) {
+  x++;
+  console.log(x);
+})(x);
+console.log(x);
