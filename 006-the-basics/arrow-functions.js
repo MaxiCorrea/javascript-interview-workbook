37b24aae0a1578493dfed072294a5f710ecd4f4f
Maximiliano Correa <jmaxicorrea@gmail.com>
2022-06-07 20:16:23 -0300

    Arrow Functions Examples

---
+++ 006-the-basics/arrow-functions.js
@@ -0,0 +1,11 @@
+// No parameters
+let greeting = () => ("Hello");
+console.log(greeting());
+
+// One parameters
+let singleVal = a => a + 1;
+console.log(singleVal(3));
+
+// Multiple parameters
+let multiply =(a, b) => {return a * b}
+console.log(multiply(2, 3));
